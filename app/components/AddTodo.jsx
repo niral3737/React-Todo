313191fc0bc0928fd566e34afec6340faa95ae5c
niral.eagle100@gmail.com <niral.eagle100@gmail.com>
2016-09-01 10:08:07 +0530

    add seach component

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+
+var AddTodo = React.createClass({
+  handleSubmit : function (e) {
+    e.preventDefault();
+
+    var text = this.refs.text.value;
+    if(text.length > 0){
+      this.refs.text.value = '';
+      this.props.onAddTodo(text);
+    }else{
+      this.refs.text.focus();
+    }
+  },
+  render: function() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="What do you need to do?" ref="text"></input>
+          <button className="button expanded">Add Todo</button>
+        </form>
+      </div>
+    );
+  }
+});
+
+module.exports = AddTodo;
--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,5 +1,7 @@
 var React = require('react');
 var TodoList = require('TodoList');
+var AddTodo = require('AddTodo');
+var TodoSearch = require('TodoSearch');
 
 var TodoApp = React.createClass({
   getInitialState : function () {
@@ -21,15 +23,28 @@ var TodoApp = React.createClass({
           id : 4,
           text : 'Go to Market'
         }
-      ]
+      ],
+      showCompleted : false,
+      searchText : ''
     };
+  },
+  handleAddTodo : function (text) {
+    alert(`new todo ${text}`);
+  },
+  handleSearch : function (showCompleted, searchText) {
+    this.setState({
+      showCompleted : showCompleted,
+      searchText : searchText.toLowerCase()
+    });
   },
   render: function() {
     var {todos} = this.state;
     return (
       <div>
         <h1>Todo App</h1>
+        <TodoSearch onSearch={this.handleSearch} />
         <TodoList todos={todos} />
+        <AddTodo onAddTodo={this.handleAddTodo} />
       </div>
     );
   }
