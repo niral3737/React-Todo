589f97c53219873f386575077a0591b1a79ab108
niral.eagle100@gmail.com <niral.eagle100@gmail.com>
2016-09-01 14:14:28 +0530

    Add search functionality

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -53,8 +53,47 @@ describe('TodoAPI', () => {
 
       expect(TodoAPI.getTodos()).toEqual(todos);
     });
+  });
 
+  describe('filterTodos', () => {
+    var todos = [{
+      id : 1,
+      text : 'test1',
+      completed : true
+    },
+    {
+      id : 2,
+      text : 'test2',
+      completed : false
+    },
+    {
+      id : 3,
+      text : 'test3',
+      completed : true
+    }];
 
+    it('should return all items if showCompleted is true', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos.length).toBe(3);
+    });
+    it('should return incomplete items if showCompleted is false', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort by completed status', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should search by searchText', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '2');
+
+      expect(filteredTodos[0].text).toBe('test2');
+    });
   });
 
 });
