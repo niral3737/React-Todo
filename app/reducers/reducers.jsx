2ab831c3c13a38806a21ab49c58d809c09728c96
niral.eagle100@gmail.com <niral.eagle100@gmail.com>
2016-09-02 17:52:32 +0530

    Add Redux

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,3 +1,6 @@
+var uuid = require('node-uuid');
+var moment = require('moment');
+
 export var searchTextReducer = (state = '', action) => {
   switch (action.type) {
     case 'SET_SEARCH_TEXT':
@@ -17,3 +20,36 @@ export var showCompletedReducer = (state = false, action) => {
       return state;
   }
 };
+
+export var todosReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, {
+        id : uuid(),
+        text : action.text,
+        completed : false,
+        createdAt : moment().unix(),
+        completedAt : undefined
+      }];
+
+    case 'TOGGLE_TODO':
+      return state.map((todo) => {
+        if(action.id === todo.id){
+
+          return {
+            ...todo,
+            completed : !todo.completed,
+            completedAt : !todo.completed ? moment().unix() : undefined
+          };
+          // todo.completed : !todo.completed;
+          // if(todo.completed){
+          //   todo.completedAt = moment().unix();
+          // }else{
+          //   todo.completedAt = undefined;
+          // }
+        }
+      });
+    default:
+      return state;
+  }
+}
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -28,4 +28,40 @@ describe('Reducers', () => {
       expect(res).toBe(true);
     });
   });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type : 'ADD_TODO',
+        text : 'Walk dog'
+      };
+
+      var res = reducers.todosReducer(deepFreeze([]), deepFreeze(action));
+
+      expect(res.length).toBe(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo', () => {
+      var todos = [
+        {
+          id : 1,
+          text : 'Walk',
+          completed : true,
+          createdAt : 123,
+          completedAt : 125
+        }
+      ];
+
+      var action = {
+        type : 'TOGGLE_TODO',
+        id : 1
+      };
+
+      var res = reducers.todosReducer(deepFreeze(todos), deepFreeze(action));
+
+      expect(res[0].completed).toBe(false);
+      expect(res[0].completedAt).toNotExist();
+    });
+  });
 });
