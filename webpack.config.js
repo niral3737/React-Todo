2dfd04481eaa54655bf3efeab70ef11c0ee8ded5
niral.eagle100@gmail.com <niral.eagle100@gmail.com>
2016-09-03 16:15:08 +0530

    Bug fix for app crash on heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
 
-
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
@@ -22,6 +21,9 @@ module.exports = {
       compressor : {
         warnings : false
       }
+    }),
+    new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     })
   ],
   output : {
